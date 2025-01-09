ea5ebc18fb5763c2df169d2f9216e2967162779f
Dan Sniderman <dan.sniderman@3cloudsolutions.com>
2025-01-09 09:09:06 -0600

    update ami and account

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
  resource "aws_instance" "ec2-instance1" {
-   ami = "ami-0005e0cfe09cc9050"
+   ami = "ami-0b4624933067d393a"
    instance_type = "t2.micro"
 
-   tags = {                                
+   tags = {                                 
      Name = "demo-server1"
 }
 }
@@ -10,7 +10,7 @@
 
 resource "aws_instance" "ec2-Instance2" {
   provider      = aws.west
-  ami           = "ami-0a5ed7a812aeb495a"  # AMI for us-west-1
+  ami           = "ami-0aa117785d1c1bfe5"  # AMI for us-west-1
   instance_type = "t2.micro"
 
   tags = {                                
