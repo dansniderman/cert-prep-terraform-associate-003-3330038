71bbbe374c675cd413c184a76481a14f5f24fbdb
Dan Sniderman <dan.sniderman@3cloudsolutions.com>
2025-01-09 10:15:21 -0600

    updates for lab

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
 resource "aws_instance" "MyInstance_test" {
     #instance configuration
+    ami = "ami-01816d07b1128cd2d"
+    instance_type = "t2.micro"
+    tags = {
+      Name = "MyInstance_test"
+    }
 }
